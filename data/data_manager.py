53f3f75dbd4671b4cc0b0a75c9679727b880d55f
Nigota <postaforjob@gmail.com>
2024-02-04 01:30:31 +0400

    реализовал функцию создания пользователя

--- data/data_manager.py
+++ data/data_manager.py
@@ -1,12 +1,23 @@
 from typing import Union
+from data import db_session
+from data.users import User
+
+db_session.global_init("db/database.db")
+db_sess = db_session.create_session()
 
 
 def get_user_password(username: str) -> Union[None, str]:
     """Должен возвращать пароль юзера по его имени, если пароля нет, то пусть возвращает None"""
 
 
-def create_user(username: str, email: str, password: str):
+def create_user(user_name: str, email: str, password: str):
     """Создает юзера в бд"""
+    user = User()
+    user.first_name_user = user_name
+    user.email = email
+    user.set_password(password)
+    db_sess.add(user)
+    db_sess.commit()
 
 
 def get_user(usesrname: str) -> Union[None, dict]:
@@ -24,4 +35,3 @@ def set_session_id(username: str, session_id: str):
 
 def delete_user(username: str):
     """Пусть удалаяет юзера нахой"""
-
