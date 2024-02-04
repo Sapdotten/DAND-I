3c628055f4f2ea075ac461fa48838aff578b5a10
Nigota <postaforjob@gmail.com>
2024-02-04 13:27:01 +0400

    проверка юзеров

--- test/users/check_balance.py
+++ test/users/check_balance.py
@@ -1,4 +1,6 @@
 from requests import post
 import json
 
-print(post())
+print(post(f'http://127.0.0.1:5000/balance', json={"user_id": 1}).json())
+print(post(f'http://127.0.0.1:5000/set_balance', json={"user_id": 1, "balance": 40}).json())
+print(post(f'http://127.0.0.1:5000/balance', json={"user_id": 1}).json())
