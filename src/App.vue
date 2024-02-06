4d3ac092c8231fe5d5e250858effdf81f20c52a9
Lolja4you <104832612+Lolja4you@users.noreply.github.com>
2024-02-07 02:15:07 +0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="app">
+        <router-link to="/">Go to Home</router-link>
+        <router-link to="/about">Go to About</router-link>
+    </div>
+</template>
+  
+<script>
+    export default {
+    name: 'app'
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+<<<<<<< HEAD
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool: 'eval',
+  devServer: {
+    port: 4444
+  },
+  module:{
+    rules:[
+      {
+        test:/\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test:/\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+=======
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  context: path.join(__dirname, 'src'),
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'dist')
+  },
+  devtool: 'eval',
+  devServer: {
+    port: 4444
+  },
+  module:{
+    rules:[
+      {
+        test:/\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test:/\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ]
+>>>>>>> 7df36cb811cd3d2431b267039ff760d05520b982
+};
